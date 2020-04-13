aabdb0c9e578ff094b2c841dbaac9375822b2c7e
Danil1702 <kalchenko@ucu.edu.ua>
2020-04-13 17:26:01 +0300

    'j'

---
+++ examples/Modules_examples/JSON_examples/json_dump.py
@@ -0,0 +1,19 @@
+import json
+
+
+data = {}
+data['people'] = []
+data['people'].append({
+    'name': 'Scott',
+    'website': 'stackabuse.com',
+    'from': 'Nebraska'
+})
+data['people'].append({
+    'name': 'Larry',
+    'website': 'google.com',
+    'from': 'Michigan'
+})
+
+
+with open('data.txt', 'w') as f:
+    json.dump(data, f)
